fe2506d5c8ed9f79e73731aa5c117515e4334e6c
alsafa012 <alsafa012@gmail.com>
2023-08-03 01:18:13 +0600

    leap-year

--- leap-year.js
+++ leap-year.js
@@ -1,43 +1,48 @@
  function leapYear(year){
-     if((year % 4 ==0 && year % 100 !=0) || year % 400 != 0){
-          return true;
+     if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
+          return "Leap-year";
      }
      else {
-          return false;
+          return "Not Leap-Year";
      }
 }
+// one option to print result
 // console.log(leapYear(2022)); 
 
-const leapYear1 = leapYear(2024)
-console.log(leapYear1);
-// const leapYear1 = 2000;
-// console.log(leapYear(leapYear1));
-// const yearToCheck = 2024; // Replace this with the year you want to check
+// 2nd option to print result
+/* const leapYear1 = leapYear(2021);
+console.log(leapYear1); */
 
-// if (leapYear(yearToCheck)) {
-//   console.log(`${yearToCheck} is a leap year.`);
-// } else {
-//   console.log(`${yearToCheck} is not a leap year.`);
-// }
+// 3rd option to print result
+
+// const leapYear1 = 2020;
+// console.log(leapYear(leapYear1));
+
+const yearToCheck = 2024; // Replace this with the year you want to check
+
+if (leapYear(yearToCheck)) {
+  console.log(`${yearToCheck} is a leap year.`);
+} else {
+  console.log(`${yearToCheck} is not a leap year.`);
+}
  
 
-
-
-// function isLeapYear(year) {
-//      if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-//        return true;
-//      } else {
-//        return false;
-//      }
-//    }
-   
-//    const yearToCheck = 2024; // Replace this with the year you want to check
-   
-//    if (isLeapYear(yearToCheck)) {
-//      console.log(`${yearToCheck} is a leap year.`);
-//    } else {
-//      console.log(`${yearToCheck} is not a leap year.`);
-//    }
    
 
-
+// chat-gpt
+/* function isLeapYear(year) {
+     if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+       return true;
+     } else {
+       return false;
+     }
+   }
+   
+   const yearToCheck = 2021; // Replace this with the year you want to check
+   
+   if (isLeapYear(yearToCheck)) {
+     console.log(`${yearToCheck} is a leap year.`);
+   } else {
+     console.log(`${yearToCheck} is not a leap year.`);
+   }
+    */
